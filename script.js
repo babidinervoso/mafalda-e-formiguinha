2ff76cbbadba5c913961a8a757a557e57c012a7a
Babidi Nervoso <jose.ramos47@aluno.ce.gov.br>
2024-10-17 09:58:34 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let filaDeEspera = [];
 
+filaDeEspera.push("Matheus");
+console.log("Matheus entrou na fila.");
+
+filaDeEspera.push("Marcos");
+console.log("Marcos entrou na fila.");
+
+filaDeEspera.push("Ana");
+console.log("Ana entrou na fila.");
+
+if (filaDeEspera.length === 0) {
+    console.log("Fila está vazia.");
+} else {
+    let consultado = filaDeEspera.shift();
+    console.log(`${consultado} saiu da fila.`);
+}
+
+if (filaDeEspera.length === 0) {
+    console.log("Fila está vazia.");
+} else {
+    let consultado = filaDeEspera.shift();
+    console.log(`${consultado} saiu da fila.`);
+}
+
+if (filaDeEspera.length === 0) {
+    console.log("Fila está vazia.");
+} else {
+    let consultado = filaDeEspera.shift();
+    console.log(`${consultado} saiu da fila.`);
+}
+
+if (filaDeEspera.length === 0) {
+    console.log("Fila está vazia.");
+} else {
+    let consultado = filaDeEspera.shift();
+    console.log(`${consultado} saiu da fila.`)}
